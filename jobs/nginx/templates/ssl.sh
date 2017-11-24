3f8a85765f5c078c354d1cf7f546e5b23f2b7988
Ben Jackson <b@benjackson.email>
2017-11-24 14:48:28 +1100

    update scripts and testing ops

--- jobs/nginx/templates/ssl.sh
+++ jobs/nginx/templates/ssl.sh
@@ -4,11 +4,11 @@ echo "Place SSL certificates if defined"
 <% if_p("ssl.certificates") do |certificates| -%>
 echo "Placing certs"
 <% for cert in certificates -%>
-cat > /var/vcap/jobs/nginx/ssl/<%= cert.name %>.crt << EOF
-<%= cert.certificate %>
+cat > /var/vcap/jobs/nginx/ssl/<%= cert["name"] %>.crt << EOF
+<%= cert["certificate"] %>
 EOF
-cat > /var/vcap/jobs/nginx/ssl/<%= cert.name %>.key << EOF
-<%= cert.key %>
+cat > /var/vcap/jobs/nginx/ssl/<%= cert["name"] %>.key << EOF
+<%= cert["key"] %>
 EOF
 <% end -%>
 echo "Done"
